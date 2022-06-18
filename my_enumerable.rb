360c7e3192ad635891cac3842893ce0b32b1ad98
Damola Adediran <tylher123@gmail.com>
2022-06-18 01:33:23 +0100

    Create enumerable module implementing any?, all? and filter methods

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,3 +1,21 @@
 module MyEnumerable
-    
+  def any?
+    return unless block_given
+
+    each { |i| return true if yield(i) }
+    false
+  end
+
+  def all?
+    return unless block_given
+
+    each { |i| return false unless yield(i) }
+    true
+  end
+
+  def filter
+    new_arr = []
+    each { |n| new_arr << n if yield(n) }
+    new_arr
+  end
 end
--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,2 @@
-class  MyList
-    
+class MyList
 end
